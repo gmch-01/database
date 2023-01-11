bd65a62ee340020415adebfb88db4d506a3f883a
Gonzalo Manuel Cuevas Hinojosa <manuelhinojosagc@gmail.com>
2023-01-11 11:47:14 -0400

    base datos actualizada

--- maxisoft_db.sql
+++ maxisoft_db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: maxisoft_db
+-- Host: localhost    Database: maxisoft_db
 -- ------------------------------------------------------
--- Server version	8.0.29
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,14 @@ DROP TABLE IF EXISTS `hoja_de_produccion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hoja_de_produccion` (
-  `id_hoja_de_produccion` int NOT NULL,
+  `id_hoja_produccion` int NOT NULL,
   `id_receta` int DEFAULT NULL,
   `cantidad` double DEFAULT NULL,
   `fecha_hoja` date DEFAULT NULL,
   `encargado` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_hoja_de_produccion`)
+  `estado` int DEFAULT NULL,
+  `progreso` int DEFAULT NULL,
+  PRIMARY KEY (`id_hoja_produccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,6 +40,7 @@ CREATE TABLE `hoja_de_produccion` (
 
 LOCK TABLES `hoja_de_produccion` WRITE;
 /*!40000 ALTER TABLE `hoja_de_produccion` DISABLE KEYS */;
+INSERT INTO `hoja_de_produccion` VALUES (90001,50001,3,'2023-05-16','Gonzalo',1,0),(90002,50002,5,'2023-09-10','qwer',1,0);
 /*!40000 ALTER TABLE `hoja_de_produccion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +65,7 @@ CREATE TABLE `insumo` (
 
 LOCK TABLES `insumo` WRITE;
 /*!40000 ALTER TABLE `insumo` DISABLE KEYS */;
-INSERT INTO `insumo` VALUES (10001,'harina','kilos'),(10002,'levadura','kilos'),(10003,'azucar','kilos');
+INSERT INTO `insumo` VALUES (10001,'harina','kilos'),(10002,'levadura','kilos'),(10003,'azucar','kilos'),(10004,'manteca','kilos'),(10005,'gluten','kilos'),(10006,'antimoho','kilos'),(10007,'lusopan','kilos');
 /*!40000 ALTER TABLE `insumo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,7 +79,7 @@ DROP TABLE IF EXISTS `inventario_insumo`;
 CREATE TABLE `inventario_insumo` (
   `id_inv_ins` int NOT NULL,
   `tipo_insumo` int DEFAULT NULL,
-  `fecha_venc` timestamp NULL DEFAULT NULL,
+  `fecha_venc` date DEFAULT NULL,
   `cantidad_actual` int DEFAULT NULL,
   PRIMARY KEY (`id_inv_ins`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -88,7 +91,7 @@ CREATE TABLE `inventario_insumo` (
 
 LOCK TABLES `inventario_insumo` WRITE;
 /*!40000 ALTER TABLE `inventario_insumo` DISABLE KEYS */;
-INSERT INTO `inventario_insumo` VALUES (20001,1001,'2023-05-20 04:00:00',100),(20002,1002,'2023-06-15 04:00:00',200);
+INSERT INTO `inventario_insumo` VALUES (60001,10001,'2023-05-20',100),(60002,10002,'2023-06-15',200),(60003,10003,'2023-04-10',50),(60004,10004,'2023-06-04',20),(60005,10005,'2023-02-06',35),(60006,10006,'2023-03-10',27),(60007,10007,'2023-04-20',42);
 /*!40000 ALTER TABLE `inventario_insumo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +117,7 @@ CREATE TABLE `inventario_producto` (
 
 LOCK TABLES `inventario_producto` WRITE;
 /*!40000 ALTER TABLE `inventario_producto` DISABLE KEYS */;
-INSERT INTO `inventario_producto` VALUES (50001,30001,'2023-05-20',150),(50002,30002,'2023-05-25',250);
+INSERT INTO `inventario_producto` VALUES (50001,20001,'2023-05-20',150),(50002,20002,'2023-05-25',250),(50003,20003,'2023-02-10',50),(50004,20004,'2023-03-15',20),(50005,20005,'2023-02-06',10);
 /*!40000 ALTER TABLE `inventario_producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,7 +224,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (20001,'pan molde','receta de pan molde'),(20002,'sarnita','receta de pan sarnita ');
+INSERT INTO `producto` VALUES (20001,'pan molde','receta de pan molde'),(20002,'sarnita','receta de pan sarnita '),(20003,'Hamburguesa','Hamburguesa con queso'),(20004,'Molde Pizza','molde dde pizza'),(20005,'Panchito',NULL);
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -247,8 +250,33 @@ CREATE TABLE `receta` (
 
 LOCK TABLES `receta` WRITE;
 /*!40000 ALTER TABLE `receta` DISABLE KEYS */;
-INSERT INTO `receta` VALUES (123,20002,10001,1234),(50001,20001,10002,5),(50002,20002,10001,3);
+INSERT INTO `receta` VALUES (50001,20001,10002,5),(50002,20002,10001,3),(50003,20001,10003,4),(50004,20002,10004,5),(12341234,20001,10002,23452345);
 /*!40000 ALTER TABLE `receta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recetas`
+--
+
+DROP TABLE IF EXISTS `recetas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recetas` (
+  `id_recetas` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `id_insumo` varchar(45) DEFAULT NULL,
+  `cantidad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_recetas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recetas`
+--
+
+LOCK TABLES `recetas` WRITE;
+/*!40000 ALTER TABLE `recetas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recetas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -292,4 +320,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-09  2:07:33
+-- Dump completed on 2023-01-11 11:46:43
