9008dadabdb6219b7c3dd4c8471529499a83b767
Gonzalo Manuel Cuevas Hinojosa <manuelhinojosagc@gmail.com>
2023-04-22 13:32:23 -0400

    database 1.0

--- maxisoft_db.sql
+++ maxisoft_db.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: localhost    Database: maxisoft_db
--- ------------------------------------------------------
--- Server version	8.0.30
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `hoja_de_produccion`
---
 
 DROP TABLE IF EXISTS `hoja_de_produccion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `hoja_de_produccion` (
   `id_hoja_produccion` int NOT NULL,
   `id_receta` int DEFAULT NULL,
@@ -33,51 +13,36 @@ CREATE TABLE `hoja_de_produccion` (
   `peso_recibido` int DEFAULT '0',
   `embolsado` int DEFAULT '0',
   PRIMARY KEY (`id_hoja_produccion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `hoja_de_produccion`
---
 
 LOCK TABLES `hoja_de_produccion` WRITE;
-/*!40000 ALTER TABLE `hoja_de_produccion` DISABLE KEYS */;
+
 INSERT INTO `hoja_de_produccion` VALUES (9019,50001,10,'2023-05-19','jhtf',NULL,70,40,86),(90001,50014,3,'2023-05-16','Gonzalo',1,100,120,20);
-/*!40000 ALTER TABLE `hoja_de_produccion` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `insumo`
---
+
 
 DROP TABLE IF EXISTS `insumo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `insumo` (
   `id_insumo` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `unidad` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_insumo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `insumo`
---
 
 LOCK TABLES `insumo` WRITE;
-/*!40000 ALTER TABLE `insumo` DISABLE KEYS */;
+
 INSERT INTO `insumo` VALUES (10001,'harina','kilos'),(10002,'levadura','kilos'),(10003,'azucar','kilos'),(10004,'manteca','kilos'),(10005,'gluten','kilos'),(10006,'antimoho','kilos'),(10007,'lusopan','kilos');
-/*!40000 ALTER TABLE `insumo` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `insumos_has_recetas`
---
 
 DROP TABLE IF EXISTS `insumos_has_recetas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `insumos_has_recetas` (
   `insumo_id_insumo` int NOT NULL,
   `recetas_id_recetas` int NOT NULL,
@@ -85,77 +50,50 @@ CREATE TABLE `insumos_has_recetas` (
   KEY `fk_insumo_has_recetas_recetas1_idx` (`recetas_id_recetas`),
   CONSTRAINT `fk_insumo_has_recetas_insumo1` FOREIGN KEY (`insumo_id_insumo`) REFERENCES `insumo` (`id_insumo`),
   CONSTRAINT `fk_insumo_has_recetas_recetas1` FOREIGN KEY (`recetas_id_recetas`) REFERENCES `receta` (`id_receta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `insumos_has_recetas`
---
 
 LOCK TABLES `insumos_has_recetas` WRITE;
-/*!40000 ALTER TABLE `insumos_has_recetas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `insumos_has_recetas` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `inventario_insumo`
---
 
 DROP TABLE IF EXISTS `inventario_insumo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `inventario_insumo` (
   `id_inv_ins` int NOT NULL,
   `tipo_insumo` int DEFAULT NULL,
   `fecha_venc` date DEFAULT NULL,
   `cantidad_actual` int DEFAULT NULL,
   PRIMARY KEY (`id_inv_ins`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `inventario_insumo`
---
+);
 
 LOCK TABLES `inventario_insumo` WRITE;
-/*!40000 ALTER TABLE `inventario_insumo` DISABLE KEYS */;
+
 INSERT INTO `inventario_insumo` VALUES (60001,10001,'2023-05-20',10),(60002,10002,'2023-06-15',200),(60003,10003,'2023-06-10',5),(60004,10004,'2023-06-04',20),(60005,10005,'2023-02-06',35),(60006,10006,'2023-03-10',27),(60007,10007,'2023-04-20',42);
-/*!40000 ALTER TABLE `inventario_insumo` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `inventario_producto`
---
-
 DROP TABLE IF EXISTS `inventario_producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `inventario_producto` (
   `id_inv_producto` int NOT NULL AUTO_INCREMENT,
   `tipo_prod` int DEFAULT NULL,
   `fecha_vencimiento` date DEFAULT NULL,
   `cantidad_actual` int DEFAULT NULL,
   PRIMARY KEY (`id_inv_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `inventario_producto`
---
 
 LOCK TABLES `inventario_producto` WRITE;
-/*!40000 ALTER TABLE `inventario_producto` DISABLE KEYS */;
+
 INSERT INTO `inventario_producto` VALUES (50001,20001,'2023-05-20',10),(50002,20002,'2023-05-25',70),(50003,20003,'2023-02-10',50),(50004,20004,'2023-03-15',30),(50005,20005,'2023-02-06',50);
-/*!40000 ALTER TABLE `inventario_producto` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `kardex_insumo`
---
 
 DROP TABLE IF EXISTS `kardex_insumo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `kardex_insumo` (
   `id_det_insumo` int NOT NULL AUTO_INCREMENT,
   `fecha_entrada` varchar(40) DEFAULT NULL,
@@ -166,26 +104,18 @@ CREATE TABLE `kardex_insumo` (
   `usuario` varchar(45) DEFAULT NULL,
   `fecha_vencimiento` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_det_insumo`)
-) ENGINE=InnoDB AUTO_INCREMENT=30004 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `kardex_insumo`
---
+);
 
 LOCK TABLES `kardex_insumo` WRITE;
-/*!40000 ALTER TABLE `kardex_insumo` DISABLE KEYS */;
+
 INSERT INTO `kardex_insumo` VALUES (30001,'2023-05-24 ','DonJose',5,10001,'485','gonzalo','2023-05-10'),(30002,'2023-05-16','Pepe',20,10002,'12344','qwerrr','2023-02-12'),(30003,'2023-06-10','pepe',5,10003,NULL,'gonzalo','2023-06-10');
-/*!40000 ALTER TABLE `kardex_insumo` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `kardex_producto`
---
+
 
 DROP TABLE IF EXISTS `kardex_producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `kardex_producto` (
   `id_det_producto` int NOT NULL AUTO_INCREMENT,
   `fecha_registro` varchar(45) DEFAULT NULL,
@@ -194,26 +124,16 @@ CREATE TABLE `kardex_producto` (
   `encargado` varchar(45) DEFAULT NULL,
   `fecha_vencimiento` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_det_producto`)
-) ENGINE=InnoDB AUTO_INCREMENT=40016 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `kardex_producto`
---
+);
 
 LOCK TABLES `kardex_producto` WRITE;
-/*!40000 ALTER TABLE `kardex_producto` DISABLE KEYS */;
+
 INSERT INTO `kardex_producto` VALUES (40001,'2023-06-17',50,20001,'Gonzalo','2023-09-16'),(40002,'2023-05-15',20,20003,'Peper','2023-05-10'),(40014,'2023-10-02',20,20002,'Jose',NULL);
-/*!40000 ALTER TABLE `kardex_producto` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `produccion`
---
-
 DROP TABLE IF EXISTS `produccion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `produccion` (
   `id_produccion` int NOT NULL,
   `id_hoja_produccion` int DEFAULT NULL,
@@ -221,25 +141,15 @@ CREATE TABLE `produccion` (
   `descripcion` varchar(45) DEFAULT NULL,
   `fecha_produccion` date DEFAULT NULL,
   PRIMARY KEY (`id_produccion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `produccion`
---
+);
 
 LOCK TABLES `produccion` WRITE;
-/*!40000 ALTER TABLE `produccion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `produccion` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `producto`
---
 
 DROP TABLE IF EXISTS `producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `producto` (
   `id_producto` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
@@ -247,52 +157,34 @@ CREATE TABLE `producto` (
   `peso_receta` double DEFAULT NULL,
   `cantidad_bolsa` int DEFAULT NULL,
   PRIMARY KEY (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `producto`
---
+) ;
 
 LOCK TABLES `producto` WRITE;
-/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+
 INSERT INTO `producto` VALUES (20001,'Pan molde','receta de pan molde',45.75,84),(20002,'Sarnita','receta de pan sarnita ',58.2,140),(20003,'Hamburguesa','Hamburguesa con queso',59.2,140),(20004,'Molde Pizza','Molde Pizza',50.3,145),(20005,'Panchito','Panchito Extra largo',55.4,140);
-/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `receta`
---
-
 DROP TABLE IF EXISTS `receta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `receta` (
   `id_receta` int NOT NULL AUTO_INCREMENT,
   `id_producto` int DEFAULT NULL,
   `id_insumo` int DEFAULT NULL,
   `cantidad` double DEFAULT NULL,
   PRIMARY KEY (`id_receta`)
-) ENGINE=InnoDB AUTO_INCREMENT=50020 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `receta`
---
+);
 
 LOCK TABLES `receta` WRITE;
-/*!40000 ALTER TABLE `receta` DISABLE KEYS */;
+
 INSERT INTO `receta` VALUES (50001,20001,10001,39),(50002,20001,10002,2),(50003,20001,10003,3),(50004,20001,10004,2),(50005,20001,10005,0.5),(50006,20001,10006,0.15),(50007,20001,10007,0.1),(50008,20002,10001,50),(50009,20002,10002,2),(50010,20002,10003,3),(50011,20002,10004,2),(50012,20002,10006,0.1),(50013,20002,10007,0.1),(50014,20003,10001,50),(50015,20003,10002,2),(50016,20003,10003,5),(50017,20003,10004,3),(50018,20003,10006,0.1),(50019,20003,10007,0.1);
-/*!40000 ALTER TABLE `receta` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `usuario`
---
+
 
 DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `usuario` (
   `ci_persona` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
@@ -305,26 +197,10 @@ CREATE TABLE `usuario` (
   `password` varchar(45) DEFAULT NULL,
   `fecha_registro` time DEFAULT NULL,
   PRIMARY KEY (`ci_persona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
+) ;
 
 LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+
 INSERT INTO `usuario` VALUES (123456,'Pepito','Grillo',1,'ya tu sabe',78852520,'masculino','pepitog','123456',NULL),(8442536,'Gonzalo','Cuevas',0,'4to centenario ',78865656,'masculino','gcuevash','123456','00:00:00');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-03-12 19:29:42
